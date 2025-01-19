ee9c1a436f25058e95443be95d6870a3c7f2e2e2
PlayableGIT <piotrsalata02@gmail.com>
2025-01-19 23:38:18 +0100

    basic combat 0.1
    -zombie i survivor mogą się atakować
    -śmierć jednego z nich nie wypierdala godota <3

--- area_2d.gd
+++ area_2d.gd
@@ -1,13 +1,13 @@
 extends Area2D
 
-var rng = RandomNumberGenerator.new()
+#var rng = RandomNumberGenerator.new()
 
 
 
-func _on_body_entered(body):
-	var rng_damage = rng.randi_range(1, 3)
-	var damage_total = 0
-	if body.name == "Zombie":
-		print(rng_damage, " DMG dealt!")	
+#func _on_body_entered(body):
+	#var rng_damage = rng.randi_range(1, 3)
+	#var damage_total = 0
+	#if body.name == "Zombie":
+		#print(rng_damage, " DMG dealt!")	
 	#damage_total = damage_total + rng_damage
 	#print("damage total:", damage_total)
---
+++ zombie.gd
@@ -0,0 +1,70 @@
+extends CharacterBody2D
+
+
+# Prędkość ruchu obiektu
+@export var speed = 350.0
+
+var direction = Vector2.ZERO
+var survivor_in_range = false
+var survivor_attack_cooldown = true
+var health = 25
+var zombie_alive = true
+
+
+func _physics_process(delta: float) -> void:
+	#grawitacja
+	if not is_on_floor():
+		velocity += get_gravity() * 10 * delta
+		velocity.x = 0.0
+		
+	var player = get_parent().find_child("Survivor")
+	if player == null:
+		pass
+	else:
+		move_and_slide()
+		moveCharacter()
+		survivor_attack()
+	
+	if health <= 0:
+		zombie_alive = false
+		health = 0
+		print("Zombie has been killed!")
+		self.queue_free()
+	
+	
+
+
+func moveCharacter():
+	#powolanie survivora
+	var player = get_parent().find_child("Survivor")
+	#kierunek
+	direction = global_position.direction_to(player.global_position)
+	
+	#predkosc w kierunku
+	velocity = direction.normalized() * speed
+	
+
+func zombie():
+	pass
+	
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.has_method("survivor"):
+		survivor_in_range = true
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.has_method("survivor"):
+		survivor_in_range = false
+		
+func survivor_attack():
+	if survivor_in_range and survivor_attack_cooldown:
+			#rng
+		var rng = RandomNumberGenerator.new()
+		var rng_damage = rng.randi_range(1, 10)	
+		survivor_attack_cooldown = false
+		$attack_cooldown.start()
+		health = health - rng_damage
+		print("Zombie took ", rng_damage, " damage! Health: ", health)
+
+
+func _on_attack_cooldown_timeout() -> void:
+	survivor_attack_cooldown = true
