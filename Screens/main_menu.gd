da7f3f640362e136eb421dbb1bc8324a5742efcb
Matsal92 <mateusz.salata29@gmail.com>
2025-01-21 17:49:28 +0100

    Menu odpala sie z projektu jak trzeba

--- Screens/main_menu.gd
+++ Screens/main_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://scena.tscn")
+	get_tree().change_scene_to_file("res://Screens/scena.tscn")
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
