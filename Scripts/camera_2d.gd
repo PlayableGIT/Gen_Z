8941829a74e00b4a357a0b5c3bde90b10734b5bc
PlayableGIT <piotrsalata02@gmail.com>
2025-01-26 23:42:01 +0100

    Combat Rework 0.7
    Bullet fall fix + camera zoom

--- Scripts/camera_2d.gd
+++ Scripts/camera_2d.gd
@@ -1,10 +1,12 @@
 extends Camera2D
 
 @export var MOVE_SPEED = 300
+@export var SCROLL_SPEED = 3
 var left_limit = Vector2(915.0, 0)
 var right_limit = Vector2(4738.0, 0)
 var up_limit = Vector2(0, -800.0)
 var down_limit = Vector2(0, -270.0)
+
 
 func _process(delta):
 	#print($".".position)
@@ -27,4 +29,15 @@ func _process(delta):
 		global_position += Vector2.DOWN * delta * MOVE_SPEED
 	else:
 		pass
-	#print($".".position)
+
+	if Input.is_action_just_released("scroll_up") and $".".zoom.x < 1.5:
+		$".".zoom.x += delta*SCROLL_SPEED
+		$".".zoom.y += delta*SCROLL_SPEED
+	else:
+		pass
+		
+	if Input.is_action_just_released("scroll_down") and $".".zoom.x > 0.7:
+		$".".zoom.x -= delta*SCROLL_SPEED
+		$".".zoom.y -= delta*SCROLL_SPEED
+	else:
+		pass
