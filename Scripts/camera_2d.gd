0cf6923020dffe53d44a02630e2e5f49525095d9
PlayableGIT <piotrsalata02@gmail.com>
2025-01-28 21:25:08 +0100

    Merge fix

--- Scripts/camera_2d.gd
+++ Scripts/camera_2d.gd
@@ -1,7 +1,7 @@
 extends Camera2D
 
 @export var MOVE_SPEED = 300
-
+@export var SCROLL_SPEED = 3
 @export var left_limit = 915.0
 @export var right_limit = 4738.0
 @export var up_limit = -900.0
@@ -26,8 +26,8 @@ func _process(delta):
 	# Ruch w dół
 	if Input.is_action_pressed("ui_down") and global_position.y < down_limit:
 		global_position.y += MOVE_SPEED * delta
-    
-    	if Input.is_action_just_released("scroll_up") and $".".zoom.x < 1.5:
+	
+	if Input.is_action_just_released("scroll_up") and $".".zoom.x < 1.5:
 		$".".zoom.x += delta*SCROLL_SPEED
 		$".".zoom.y += delta*SCROLL_SPEED
 	else:
