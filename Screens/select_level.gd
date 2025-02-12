8a838d7f5eb01037382b47237ea22f5700706a7e
Matsal92 <mateusz.salata29@gmail.com>
2025-02-12 18:22:45 +0100

    select_level1

--- Screens/select_level.gd
+++ Screens/select_level.gd
@@ -3,9 +3,24 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	#get_viewport().size = DisplayServer.screen_get_size()
+	pass
+
+
+#func _on_tutorial_pressed() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_level_1_pressed() -> void:
+	$Button_Sound.play()
+	await get_tree().create_timer(0.229).timeout
+	get_tree().change_scene_to_file("res://Screens/level_1.tscn")
+
+#func _on_level_2_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_back_pressed() -> void:
+	$Button_Sound.play()
+	await get_tree().create_timer(0.229).timeout
+	get_tree().change_scene_to_file("res://Screens/Main_menu.tscn")
