97435d2a441c9b1dd94336f600a6b37cab96d40c
PlayableGIT <piotrsalata02@gmail.com>
2025-02-19 20:27:16 +0100

    Level Select fix

--- Screens/main_menu.gd
+++ Screens/main_menu.gd
@@ -6,7 +6,9 @@ func _ready() -> void:
 func _on_play_pressed() -> void:
 	$Button_Sound.play()
 	await get_tree().create_timer(0.229).timeout
-	get_tree().change_scene_to_file("res://Screens/select_level.tscn")
+	#get_tree().change_scene_to_file("res://Screens/select_level.tscn")
+	$Select_level.visible = true
+	$VBoxContainer.visible = false
 
 func _on_exit_pressed() -> void:
 	$Button_Sound.play()
