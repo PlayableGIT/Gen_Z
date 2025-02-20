51356d336994ddcdfb0459718cde09c880b3e96b
PlayableGIT <piotrsalata02@gmail.com>
2025-02-20 17:08:38 +0100

    level 2.3

--- Scripts/dead_zombie.gd
+++ Scripts/dead_zombie.gd
@@ -3,9 +3,11 @@ extends AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	$blood_splatter.visible = true
+	$blood_splatter.one_shot = true
+	$blood_splatter.emitting = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta) -> void:
 	pass
