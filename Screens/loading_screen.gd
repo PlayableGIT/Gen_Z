e22a60f3019d97be14c5c7f037d2394ae39f2321
Matsal92 <mateusz.salata29@gmail.com>
2025-02-23 12:36:49 +0100

    update Loading_screen1.0

--- Screens/loading_screen.gd
+++ Screens/loading_screen.gd
@@ -5,6 +5,9 @@ extends Control
 
 var Scene_path : String
 var progress : Array
+
+var update : float = 0.0
+
 func _ready():
 	Scene_path = "res://Screens/level_1.tscn"
 	ResourceLoader.load_threaded_request(Scene_path)
@@ -13,12 +16,18 @@ func _ready():
 func _process(delta):
 	ResourceLoader.load_threaded_get_status(Scene_path, progress)
 	
-	
-	loading_bar.value = progress[0]
-	
+	if progress[0] > update:
+		update = progress[0]
 	
 	if loading_bar.value >= 1.0:
-		get_tree().change_scene_to_packed(
+		if update >= 1.0:
+			get_tree().change_scene_to_packed(
 			ResourceLoader.load_threaded_get(Scene_path)
-		)
-		
+			)
+
+	if loading_bar.value < update:
+		loading_bar.value = lerp(loading_bar.value, update, delta)
+	loading_bar.value += delta * 0.2 * \
+	 	(0.5 if update >= 1.0 else clamp(0.9 - loading_bar.value, 0.0, 1.0))
+	
+	
